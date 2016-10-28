5ae7a99ecc2733e2805f406206e99f57f1dd2369
Cyrill Stoffel <cyrill.93@hotmail.com>
2016-10-28 14:14:25 +0200

    Neue Task hinzugefügt Led1 und Led2

--- INTRO_Common/RTOS.c
+++ INTRO_Common/RTOS.c
@@ -14,18 +14,34 @@
 #include "Application.h"
 
 static void AppTask(void* param) {
-  const int *whichLED = (int*)param;
-
-  (void)param; /* avoid compiler warning */
   for(;;) {
-    if (*whichLED==1) {
-      LED1_Neg();
-    } else if (*whichLED==2) {
-      LED2_Neg();
-    }
+	#if PL_CONFIG_HAS_KEYS
+  	  #if PL_CONFIG_HAS_DEBOUNCE
+    	KEYDBNC_Process();
+  	  #else
+    	KEY_Scan();
+  	  #endif
+	#endif
+#if PL_CONFIG_HAS_EVENTS
+    EVNT_HandleEvent(APP_EventHandler, TRUE);
+#endif
     /* \todo handle your application code here */
-    //FRTOS1_vTaskDelay(pdMS_TO_TICKS(500));
+    FRTOS1_vTaskDelay(pdMS_TO_TICKS(10));
   }
+}
+
+static void LedTask(void* param){
+	const int *whichLED = (int*)param;
+
+	(void)param;/* avoid compiler warning */
+	for(;;){
+		if (*whichLED==1) {
+		      LED1_Neg();
+		    } else if (*whichLED==2) {
+		      LED2_Neg();
+		    }
+	}
+	FRTOS1_vTaskDelay(pdMS_TO_TICKS(500));
 }
 
 void RTOS_Init(void) {
@@ -34,9 +50,15 @@ void RTOS_Init(void) {
 
   EVNT_SetEvent(EVNT_STARTUP); /* set startup event */
   /*! \todo Create tasks here */
-  if (FRTOS1_xTaskCreate(AppTask, (signed portCHAR *)"App1", configMINIMAL_STACK_SIZE, (void*)&led1, tskIDLE_PRIORITY, NULL) != pdPASS) {
+  if (FRTOS1_xTaskCreate(AppTask, (signed portCHAR *)"App", configMINIMAL_STACK_SIZE, NULL , tskIDLE_PRIORITY, NULL) != pdPASS) {
     for(;;){} /* error case only, stay here! */
   }
+  if(FRTOS1_xTaskCreate(LedTask, (signed portCHAR *) "LED1", configMINIMAL_STACK_SIZE, (void*)&led1, tskIDLE_PRIORITY, NULL) != pdPASS){
+	  for(;;){}
+  }
+  if(FRTOS1_xTaskCreate(LedTask, (signed portCHAR *) "LED2", configMINIMAL_STACK_SIZE, (void*)&led2, tskIDLE_PRIORITY, NULL) != pdPASS){
+  	  for(;;){}
+    }
 }
 
 void RTOS_Deinit(void) {
